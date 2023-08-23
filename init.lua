bacec146aec9686ae6000ff2a2f6660878b1b779
moamenhredeen <moamenhredeen@gmail.com>
2023-08-23 06:49:43 +0200

    update neovim config§

--- init.lua
+++ init.lua
@@ -25,7 +25,7 @@ vim.o.cursorline = true
 vim.o.hlsearch = false
 
 -- Make line numbers default
-vim.wo.number = true
+vim.o.number = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
@@ -65,7 +65,24 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+
+-- window navigation
+vim.keymap.set('n', '<M-j>', '<C-w>j')
+vim.keymap.set('n', '<M-k>', '<C-w>k')
+vim.keymap.set('n', '<M-h>', '<C-w>h')
+vim.keymap.set('n', '<M-l>', '<C-w>l')
+vim.keymap.set('n', '<M-l>', '<C-w>l')
+vim.keymap.set('n', '<M-q>', '<C-w>q')
+vim.keymap.set('n', '<M-o>', '<C-w>o')
+vim.keymap.set('n', '<M-s>', '<C-w>s')
+vim.keymap.set('n', '<M-v>', '<C-w>v')
+vim.keymap.set('n', '<M-r>', '<C-w>r')
+vim.keymap.set('n', '<M-n>', ':windo wincmd H<CR>')
+vim.keymap.set('n', '<M-m>', ':windo wincmd K<CR>')
+
+-- TODO: faster navigation
 
 
 -- *************************************************
@@ -74,16 +91,25 @@ vim.g.maplocalleader = ' '
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 vim.fn.system({
-"git",
-"clone",
-"--filter=blob:none",
-"https://github.com/folke/lazy.nvim.git",
-"--branch=stable", -- latest stable release
-lazypath,
+	"git",
+	"clone",
+	"--filter=blob:none",
+	"https://github.com/folke/lazy.nvim.git",
+	"--branch=stable", -- latest stable release
+	lazypath,
 })
 end
 vim.opt.rtp:prepend(lazypath)
 
+
+
+-- *************************************************
+-- configure theme 
+--
+local configure_theme = function ()
+	vim.o.background = "dark"
+	vim.cmd([[colorscheme gruvbox]])
+end
 
 
 
@@ -103,55 +129,96 @@ end
 
 
 
+-- *************************************************
+-- telescope 
+-- See :help telescope and :help telescope.setup()
+
+local configure_telescope = function ()
+	local telescope = require('telescope')
+	telescope.setup {
+	  defaults = {
+	    mappings = {
+	      i = {
+		['<C-u>'] = false,
+		['<C-d>'] = false,
+	      },
+	    },
+	  },
+	}
+
+	local fuzzy_find_buffer = function()
+	  require('telescope.builtin')
+	      .current_buffer_fuzzy_find(require('telescope.themes')
+		.get_dropdown { previewer = false })
+	end
+
+	-- Enable telescope fzf native, if installed
+	pcall(telescope.load_extension, 'fzf')
+
+	-- See `:help telescope.builtin`
+	local telescope_builtin = require('telescope.builtin')
+	-- vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+	vim.keymap.set('n', '<leader>f',  telescope_builtin.find_files, 	{ desc = 'open [F]ile' })
+	vim.keymap.set('n', '<leader>b',  telescope_builtin.buffers, 		{ desc = 'open [B]uffer' })
+	vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, 		{ desc = '[H]elp' })
+	vim.keymap.set('n', '<leader>sc', telescope_builtin.commands, 		{ desc = '[C]ommands' })
+	vim.keymap.set('n', '<leader>p',  telescope_builtin.commands, 		{ desc = '[C]ommands' })
+	vim.keymap.set('n', '<leader>sm', telescope_builtin.marks, 		{ desc = '[S]earch [D]iagnostics' })
+	vim.keymap.set('n', '<leader>sq', telescope_builtin.quickfix, 		{ desc = '[S]earch [D]iagnostics' })
+	vim.keymap.set('n', '<leader>sd', telescope_builtin.diagnostics, 	{ desc = '[S]earch [D]iagnostics' })
+	vim.keymap.set('n', '<leader>sw', telescope_builtin.live_grep, 		{ desc = '[S]earch by [G]rep' })
+	vim.keymap.set('n', '<leader>sb', fuzzy_find_buffer, 			{ desc = '[/] Fuzzily search in current buffer]' })
+end
+
+
 -- ***********************************************************************
 -- ***
 -- *** auto completion
 -- ***
 
 local configure_cmp = function()
-local cmp = require('cmp')
-local luasnip = require('luasnip')
+	local cmp = require('cmp')
+	local luasnip = require('luasnip')
 
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-	cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-	luasnip.expand_or_jump()
-      else
-	fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-	cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-	luasnip.jump(-1)
-      else
-	fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'path' },
-  },
-}
-
+	cmp.setup {
+	  snippet = {
+	    expand = function(args)
+	      luasnip.lsp_expand(args.body)
+	    end,
+	  },
+	  mapping = cmp.mapping.preset.insert {
+	    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+	    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+	    ['<C-Space>'] = cmp.mapping.complete {},
+	    ['<CR>'] = cmp.mapping.confirm {
+	      behavior = cmp.ConfirmBehavior.Replace,
+	      select = true,
+	    },
+	    ['<Tab>'] = cmp.mapping(function(fallback)
+	      if cmp.visible() then
+		cmp.select_next_item()
+	      elseif luasnip.expand_or_jumpable() then
+		luasnip.expand_or_jump()
+	      else
+		fallback()
+	      end
+	    end, { 'i', 's' }),
+	    ['<S-Tab>'] = cmp.mapping(function(fallback)
+	      if cmp.visible() then
+		cmp.select_prev_item()
+	      elseif luasnip.jumpable(-1) then
+		luasnip.jump(-1)
+	      else
+		fallback()
+	      end
+	    end, { 'i', 's' }),
+	  },
+	  sources = {
+	    { name = 'nvim_lsp' },
+	    { name = 'luasnip' },
+	    { name = 'path' },
+	  },
+	}
 end
 
 
@@ -268,12 +335,12 @@ end;
 local configure_gitsigns = function()
 	require('gitsigns').setup {
 	  signs = {
-	    add          = { hl = 'GitSignsAdd', text = '█', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-	    change       = { hl = 'GitSignsChange', text = '█', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-	    delete       = { hl = 'GitSignsDelete', text = '█', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-	    topdelete    = { hl = 'GitSignsDelete', text = '█', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-	    changedelete = { hl = 'GitSignsChange', text = '█', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-	    untracked    = { hl = 'GitSignsAdd', text = '█', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+	    add          = { hl = 'GitSignsAdd', 	text = '█', numhl = 'GitSignsAddNr', 	linehl = 'GitSignsAddLn' 	},
+	    change       = { hl = 'GitSignsChange', 	text = '█', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' 	},
+	    delete       = { hl = 'GitSignsDelete', 	text = '█', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' 	},
+	    topdelete    = { hl = 'GitSignsDelete', 	text = '█', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' 	},
+	    changedelete = { hl = 'GitSignsChange', 	text = '█', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' 	},
+	    untracked    = { hl = 'GitSignsAdd', 	text = '█', numhl = 'GitSignsAddNr', 	linehl = 'GitSignsAddLn' 	},
 	  },
 	}
 end
@@ -307,94 +374,58 @@ local configure_nvimtree = function()
 end
 
 
+
+-- ***********************************************************************
+-- configure comment 
+--
+
+local configure_comment = function ()
+	require('Comment').setup {
+	  padding = true,
+	  sticky = true,
+	  ignore = '^$',
+	  toggler = {
+	    line = 'gcc',
+	    block = 'gbc'
+	  },
+	  extra = {
+	    ---Add comment on the line above
+	    above = 'gck',
+	    ---Add comment on the line below
+	    below = 'gcj',
+	    ---Add comment at the end of line
+	    eol = 'gca',
+	  },
+	  ---Enable keybindings
+	  ---NOTE: If given `false` then the plugin won't create any mappings
+	  mappings = {
+	    ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+	    basic = true,
+	    ---Extra mapping; `gco`, `gcO`, `gcA`
+	    extra = true,
+	  },
+	  ---Function to call before (un)comment
+	  -- pre_hook = nil,
+	  ---Function to call after (un)comment
+	  -- post_hook = nil
+	}
+end
+
+
 -- *************************************************
 -- install plugisn and apply configuratio
 --
 require("lazy").setup({
-
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		config = function ()
-			vim.o.background = "dark"
-			vim.cmd([[colorscheme gruvbox]])
-		end
-	},
-
-	-- tree sitter plugin
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		config = configure_treesitter
-	},
-
-
-	-- telescope plugin
-	{
-	    "nvim-telescope/telescope.nvim"
-    	},
-
-
-	-- auto completion
-	{
-	    'hrsh7th/nvim-cmp',
-	    config = configure_cmp,
-	    dependencies = {
-	      'hrsh7th/cmp-path',
-	      'hrsh7th/cmp-nvim-lsp',
-	      'L3MON4D3/LuaSnip',
-	      'saadparwaiz1/cmp_luasnip'
-	    },
-	},
-
-	-- lsp-config plugin
-	{
-		"neovim/nvim-lspconfig", 
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim"
-		},
-		lazy = false,
-		config = configure_lspconfig
-	},
-
-	-- gitsigns plugin
-	{
-		'lewis6991/gitsigns.nvim',
-		lazy = false,
-		config = configure_gitsigns
-	},
-
-	{
-		"NeogitOrg/neogit",
-		lazy = false,
-		dependencies = {
-		"nvim-lua/plenary.nvim",
-		"sindrets/diffview.nvim",
-		},
-		config = true
-	},
-
-	{
-		 'numToStr/Comment.nvim',
-		 lazy = false
-	},
-
-	-- nvim tree
-	{
-		'nvim-tree/nvim-tree.lua',
-		config = configure_nvimtree,
-		lazy = true,
-		dependencies = {
-			'nvim-tree/nvim-web-devicons'
-		}
-	},
-
-	-- which key plugin
-	{
-		"folke/which-key.nvim",
-		lazy = true
-	},
+	{ "ellisonleao/gruvbox.nvim",  		config = configure_theme, 	lazy = false, priority = 2000 },
+	{ 'nvim-telescope/telescope.nvim',  	config = configure_telescope, 	lazy = false, priority = 1000 },
+	{ "nvim-treesitter/nvim-treesitter", 	config = configure_treesitter, 	lazy = false },
+	{ 'hrsh7th/nvim-cmp', 			config = configure_cmp, 	lazy = true, dependencies = { 'hrsh7th/cmp-path', 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' }},
+	{ "neovim/nvim-lspconfig", 		config = configure_lspconfig, 	lazy = false, dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim" }},
+	{ 'lewis6991/gitsigns.nvim', 		config = configure_gitsigns, 	lazy = false },
+	{ "NeogitOrg/neogit",  			config = true, 			lazy = false, dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" }},
+	{ 'numToStr/Comment.nvim', 		config = configure_comment , 	lazy = false },
+	{ 'windwp/nvim-autopairs', 		config = true, 			lazy = true  },
+	{ 'nvim-tree/nvim-tree.lua', 		config = configure_nvimtree, 	lazy = true, dependencies = { 'nvim-tree/nvim-web-devicons' }},
 })
 
 
