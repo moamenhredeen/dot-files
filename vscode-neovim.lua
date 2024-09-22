a6d2d4117c875bfe8d2e28ac3a6c8b12fb6bc973
Moamen Hraden <moamen.hraden@primion.eu>
2024-09-23 01:26:48 +0200

    configure vscode neovim extension

--- vscode-neovim.lua
+++ vscode-neovim.lua
@@ -9,3 +9,23 @@ end)
 vim.keymap.set('n', '<Leader>b', function ()
     vscode.action('workbench.action.quickOpen')
 end)
+
+
+vim.keymap.set('n', '<Leader>x', function ()
+    vscode.action('workbench.action.showCommands')
+end)
+
+
+vim.keymap.set('n', '<Leader>e', function ()
+    vscode.action('workbench.view.explorer')
+end)
+
+
+vim.keymap.set('n', '<Leader>o', function ()
+    vscode.action('workbench.action.gotoSymbol')
+end)
+
+
+vim.keymap.set('n', 'gs', function ()
+    vscode.action('workbench.action.gotoSymbol')
+end)
